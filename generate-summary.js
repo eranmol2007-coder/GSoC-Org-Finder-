548279a0566ba4db85b3fc74c6fd5499be06490a
eranmol2007-coder <eranmol2007@gmail.com>
2026-05-29 19:51:10 +0530

    fix: correct DATA_DIR path and fix if-block indentation
    Signed-off-by: eranmol2007-coder <eranmol2007@gmail.com>

--- generate-summary.js
+++ generate-summary.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const DATA_DIR = './data';
 const ISSUES_FILE = path.join(DATA_DIR, 'issues.json');
